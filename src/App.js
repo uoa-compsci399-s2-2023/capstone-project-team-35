e77a6839e687adb1950b758a659f22b975944cb3
finnMSY <102123775+finnMSY@users.noreply.github.com>
2023-08-08 16:52:19 +1200

    React file structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './App.css'
+import { Article } from './components '
+ 
+const App = () => {
+  return (
+    <div className='App'>
+  
+    </div>
+  )
+}
+
+export default App
