9a782e81cd0771c9412cc3905f9ff9f53f0e707d
Danila <dkho655@aucklanduni.ac.nz>
2023-09-06 01:35:15 +1200

    Extend the API layer to classify images

--- flask-backend/app/endpoints/upload_and_get_predictions/services.py
+++ flask-backend/app/endpoints/upload_and_get_predictions/services.py
@@ -1,10 +1,41 @@
-import os
-from app.adapters.repository import AbstractRepository
-from utils import get_project_root
-from pathlib import Path
+import app.globals as globals
+from app.storage.abstractrepository import AbstractRepository
+from app.ml.classifier import Classifier
+import app.ml.utilities.standardise_images as si
 from werkzeug.datastructures import FileStorage
+from typing import Dict
 
-UPLOAD_IMAGES_DIRECTORY = get_project_root() / "app" / "upload" / "uploads"
+def store_user_uploaded_images(images: list[FileStorage], repo: AbstractRepository):
+    for image in images:
+        repo.add_image(image)
 
-def store_user_uploaded_image(image: FileStorage, repo: AbstractRepository):
-    repo.add_image(image)
+def get_predictions(images: list[FileStorage], insect_type: str, model_type: str, repo: AbstractRepository) -> Dict[str, float]: 
+    store_user_uploaded_images(images, repo)
+    if model_type is None:
+        model_type = globals.DEFAULT_MODEL_TYPE
+
+    model_path = globals.ML_MODELS_DIRECTORY / insect_type / model_type.lower() / "model.h5"
+    labels_path = globals.ML_MODELS_DIRECTORY / insect_type / "labels" / "labels.csv"
+    uploaded_images_directory_path = globals.USER_UPLOADED_IMAGES_DIRECTORY
+    standardized_images_directory_path = globals.STANDARDIZED_IMAGES_DIRECTORY
+    
+    si.standardise_images(uploaded_images_directory_path, standardized_images_directory_path / "Images")
+    model = Classifier(model_path, model_type, labels_path)
+    labels, predictions, image_files, model = model.predict(standardized_images_directory_path)
+    
+    label_prediction_dict = {}
+
+    # Iterate through labels and predictions and create the dictionary
+    for label, prediction in zip(labels, predictions[0]):
+        label_prediction_dict[label] = prediction
+            
+    # Sort the dictionary items based on their values in descending order
+    sorted_prediction_values = sorted(label_prediction_dict.items(), key=lambda item: item[1], reverse=True)
+
+    # Convert the sorted items back into a dictionary
+    sorted_predictions_dict = dict(sorted_prediction_values)
+    
+    repo.clear_directory(uploaded_images_directory_path)
+    repo.clear_directory(standardized_images_directory_path / "Images") #TODO: get rid of hardcoded "Images"
+    
+    return sorted_predictions_dict
--- flask-backend/app/endpoints/upload_and_get_predictions/upload_and_get_predictions.py
+++ flask-backend/app/endpoints/upload_and_get_predictions/upload_and_get_predictions.py
@@ -1,17 +1,21 @@
 from flask import Blueprint, request
-import app.upload_and_get_predictions.services as services
-import app.adapters.repository as repo
+import app.endpoints.upload_and_get_predictions.services as services
+import app.storage.abstractrepository as repo
+import json
 
 upload_blueprint = Blueprint('upload_and_get_classifications_bp', __name__)
 
 @upload_blueprint.route('/classify', methods=['POST'])
 def upload_and_get_classifications():
+    insect_type = "trupanea" #TODO: get this from frontend
+    model_type = None        #TODO: could also add model selection to frontend
+    
     if 'image' in request.files:
-        target_image = request.files['image']  # Get the uploaded image from the front-end form
-        # services.store_user_uploaded_image(image, repo.repo_instance)
-            # classifier = new models.InsectIdentificationModel()
-            # results = classifier.predict(image)
-            # return results
-        return 'Image uploaded successfully!', 200
+        target_image = request.files['image']
+        target_image_list = [target_image]
+        species_probability_dict = services.get_predictions(target_image_list, insect_type, model_type, repo.repo_instance)   #TODO: enable upload of multiple images
+        json_serializable_return_dict = {key: str(value) for key, value in species_probability_dict.items()}
+        json_output = json.dumps(json_serializable_return_dict)
+        return json_output, 200
     else:
         return 'No image provided', 400
