4059fe3401eb4fc445e72998e77284a8506ef28a
Tommo9182 <tclu830@aucklanduni.ac.nz>
2023-09-07 21:59:07 +1200

    Updated domain tests for new classifier

--- flask-backend/tests/unit/test_domainmodel.py
+++ flask-backend/tests/unit/test_domainmodel.py
@@ -1,78 +1,49 @@
 import pytest
 import os
-
-from app.domainmodel.classifier import Classifier
+from pathlib import Path, PurePath
+from app.ml.classifier import Classifier
 
 class TestClassifier:
     def test_construction(self):
-        classifier1 = Classifier(1, 'file1', 'classifier1', 'insect1')
-        assert str(classifier1) == "<Classifier classifier1, Insect: insect1, Filename: file1, ID: 1>"
-        classifier2 = Classifier(2, 'file2', 'classifier2', 'insect2')
-        assert str(classifier2) == "<Classifier classifier2, Insect: insect2, Filename: file2, ID: 2>"
-        classifier3 = Classifier(3, 'file3', 'classifier3', 'insect3')
-        assert str(classifier3) == "<Classifier classifier3, Insect: insect3, Filename: file3, ID: 3>"
-
-        #Test non int ID
-        with pytest.raises(ValueError):
-            Classifier(None, 'file4', 'classifier4', 'insect4')
-        
-        with pytest.raises(ValueError):
-            Classifier("ID", 'file4', 'classifier4', 'insect4')
-
-        #Test negative ID
-        with pytest.raises(ValueError):
-            Classifier(-4, 'file4', 'classifier4', 'insect4')
+        #<Classifier {self.__classifier_name}, Name {self.__classifier_name}, File path {self.__classifier_path}, Labels path {self.__labels_path}>
+        classifier1 = Classifier(Path('file'), 'classifier1', Path('file'))
+        assert str(classifier1) == "<Classifier classifier1, Name classifier1, File path file, Labels path file>"
 
         #Test strings with trailing spaces
-        classifier4 = Classifier(4, '    file4   ', '   classifier4   ', '   insect4   ')
-        assert classifier4.classifier_file == 'file4'
-        assert classifier4.classifier_name == 'classifier4'
-        assert classifier4.insect_type == 'insect4'
+        classifier2 = Classifier(Path('file'), '   classifier2   ', Path('file'))
+        assert classifier2.classifier_path == Path('file')
+        assert classifier2.classifier_name == 'classifier2'
+        assert classifier2.labels_path == Path('file')
 
         #Test invalid strings
-        classifier5 = Classifier(5, 10, 20, 1029)
-        assert classifier5.classifier_file is None
-        assert classifier5.classifier_name is None
-        assert classifier5.insect_type is None
+        classifier3 = Classifier(10, 20, 1029)
+        assert classifier3.classifier_path is None
+        assert classifier3.classifier_name is None
+        assert classifier3.labels_path is None
     
     def test_setters(self):
-        classifier1 = Classifier(1, 'file1', 'classifier1', 'insect1')
+        classifier1 = Classifier(Path('file'), 'classifier1', Path('file'))
         
-        #Test classifier file name setter
-        classifier1.classifier_file = 'file2'
-        assert classifier1.classifier_file == 'file2'
+        #Test classifier path setter
 
-        classifier1.classifier_file = '   file3    '
-        assert classifier1.classifier_file == 'file3'
+        assert isinstance(Path('file2'), Path)
+        classifier1.classifier_path = Path('file2')
+        assert classifier1.classifier_path == Path('file2')
 
-        classifier1.classifier_file = 123
-        assert classifier1.classifier_file is None
+        classifier1.classifier_path = 123
+        assert classifier1.classifier_path is None
 
-        #Test classifier name setter
-        classifier1.classifier_name = 'classifier2'
-        assert classifier1.classifier_name == 'classifier2'
+        #Test labels path setter
+        classifier1.labels_path = Path('file2')
+        assert classifier1.labels_path == Path('file2')
 
-        classifier1.classifier_name = '   classifier3    '
-        assert classifier1.classifier_name == 'classifier3'
-
-        classifier1.classifier_name = 123
-        assert classifier1.classifier_name is None
-
-
-        #Test insect type setter
-        classifier1.insect_type = 'insect2'
-        assert classifier1.insect_type == 'insect2'
-
-        classifier1.insect_type = '   insect3    '
-        assert classifier1.insect_type == 'insect3'
-
-        classifier1.insect_type = 123
-        assert classifier1.insect_type is None
+        classifier1.labels_path = 123
+        assert classifier1.labels_path is None
     
     def test_equality(self):
-        classifier1 = Classifier(1, '1', '1', '1')
-        classifier2 = Classifier(2, '2', '2', '2')
-        classifier3 = Classifier(2, '2', '2', '2')
+        classifier1 = Classifier(Path('file'), 'different', Path('file'))
+        classifier2 = Classifier(Path('file'), 'same', Path('file'))
+        classifier3 = Classifier(Path('file2'), 'same', Path('file2'))
 
         #Check equality of same classifier
         assert classifier1 == classifier1
@@ -88,9 +59,9 @@ class TestClassifier:
         assert classifier1 is not None
 
     def test_sorting(self):
-        classifier1 = Classifier(1, '1', '1', '1')
-        classifier2 = Classifier(2, '2', '2', '2')
-        classifier3 = Classifier(3, '3', '3', '3')
+        classifier1 = Classifier(Path('file'), '1', Path('file'))
+        classifier2 = Classifier(Path('file'), '2', Path('file'))
+        classifier3 = Classifier(Path('file'), '3', Path('file'))
 
         #Test inequality comparison
         assert classifier1 < classifier2
@@ -102,9 +73,9 @@ class TestClassifier:
         assert sorted(classifiers) == [classifier1, classifier2, classifier3]
 
     def test_set(self):
-        classifier1 = Classifier(1, '1', '1', '1')
-        classifier2 = Classifier(2, '2', '2', '2')
-        classifier3 = Classifier(3, '3', '3', '3')
+        classifier1 = Classifier(Path('file'), '1', Path('file'))
+        classifier2 = Classifier(Path('file'), '2', Path('file'))
+        classifier3 = Classifier(Path('file'), '3', Path('file'))
 
         classifiers = set()
 
