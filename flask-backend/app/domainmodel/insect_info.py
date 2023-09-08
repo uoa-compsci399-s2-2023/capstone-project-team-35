09317bed2b3317f6bc73621a95574621dad3e4a2
Foxdapple <110433393+Foxdapple@users.noreply.github.com>
2023-09-08 12:16:26 +1200

    Implemented insect_info
    Implemented methods and functions for the insect_info class

---
+++ flask-backend/app/domainmodel/insect_info.py
@@ -0,0 +1,35 @@
+class insect:
+    def __init__(self, label, country, genus, species, image_file) -> None:
+        self.__label = label
+        self.__country = country
+        self.__genus = genus
+        self.__species = species
+        self.__image_file = image_file
+    
+    @property
+    def label(self) -> str:
+        return self.__label
+    
+    @property
+    def country(self) -> str:
+        return self.__country
+    
+    @property
+    def genus(self) -> str:
+        return self.__genus
+    
+    @property
+    def species(self) -> str:
+        return self.__species
+    
+    @property
+    def image_file(self) -> str:
+        return self.__image_file
+    
+    def __repr__(self) -> str:
+        return f"<Insect {self.label}, species = {self.species}>"
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return self.label == other.label
