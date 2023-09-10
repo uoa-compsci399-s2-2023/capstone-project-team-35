ca79fa0f64294139d73e2b5cfc7457a30556ae4c
Fiona Marie Bautista <fbau775@aucklanduni.ac.nz>
2023-09-11 10:10:54 +1200

    embedded assets trial

--- src/containers/title/Title.jsx
+++ src/containers/title/Title.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./title.css";
-import logo from "./logo.png";
+// import logo from "./logo.png";
+import main_logo from "../../assets/branding/main_logo.svg";
 
 const Title = () => {
   return (
     <div>
-      <img src={logo} />
+      <img src={main_logo} />
       <h3>your biosecurity buddy</h3>
     </div>
   );
