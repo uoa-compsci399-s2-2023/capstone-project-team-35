d804375bca6fb0bae47a259202e9b06f2c6eea67
Foxdapple <110433393+Foxdapple@users.noreply.github.com>
2023-09-12 16:19:37 +1200

    Implemented get_insect_types endpoint

--- flask-backend/app/endpoints/get_insect_type/get_insect_types.py
+++ flask-backend/app/endpoints/get_insect_type/get_insect_types.py
@@ -3,15 +3,25 @@ import app.storage.abstractrepository as repo
 from app.ml.utilities import model_output_processors as utils
 import app.globals as globals
 import json
+import os
 
 upload_blueprint = Blueprint('get_insect_types_bp', __name__)
 
 @upload_blueprint.route('/get_insect_types', methods=['POST'])
 def get_insect_types():
-    pass
-    # path = "flask-backend\app\ml\models"
-    # dir_list = os.listdir(path)
-    # if len() > 0:
-    #     return label_dictionary, 200
-    # else:
-    #     return 'No image provided', 400
+    path = os.getcwd() + "\\app" + "\ml" + "\models"
+    dir_list = os.listdir(path)
+    
+    dictonary_list = []
+    if len(dir_list) > 0:
+        for folder in dir_list:
+            label = folder[0].upper() + folder[1:]
+            label_dictionary = {
+                "label": label,
+                "value": folder
+                }
+        
+        dictonary_list.append(label_dictionary)
+        return dictonary_list, 200
+    else:
+        return 'No labels found', 400
