1d7003cec9f14e76a5065b01c7c62b2ee225f14d
Tommo9182 <tclu830@aucklanduni.ac.nz>
2023-09-16 15:38:59 +1200

    Testing of get_insect_types endpoint

--- flask-backend/tests/conftest.py
+++ flask-backend/tests/conftest.py
@@ -4,6 +4,9 @@ import app.globals as globals
 from app import create_app
 from app.storage.local.local_repository import LocalRepository
 
+'''
+To run tests, do 'pytest flask-backend/tests/unit' from project root
+'''
 
 @pytest.fixture
 def local_repo():
@@ -20,5 +23,5 @@ def end_of_tests():
     yield
     if os.path.isfile(globals.USER_UPLOADED_IMAGES_DIRECTORY / "sample_image.jpg"):
         os.remove(globals.USER_UPLOADED_IMAGES_DIRECTORY / "sample_image.jpg")
-    #if os.path.isfile(globals.STANDARDIZED_IMAGES_DIRECTORY / "sample_image.jpg"):
-        #os.remove(globals.STANDARDIZED_IMAGES_DIRECTORY / "sample_image.jpg")
+    if os.path.isfile(globals.STANDARDIZED_IMAGES_DIRECTORY / "sample_image.jpg"):
+        os.remove(globals.STANDARDIZED_IMAGES_DIRECTORY / "sample_image.jpg")
--- flask-backend/tests/unit/test_endpoints.py
+++ flask-backend/tests/unit/test_endpoints.py
@@ -1,14 +1,16 @@
+import app.globals as globals
 from utils import get_project_root
 from PIL import Image
 import os
 
 TEST_IMAGES_DIRECTORY = get_project_root() / "tests" / "test_images" 
 
-def test_upload_and_get_predictions(client, local_repo):
+def test_upload_and_get_predictions(client):
 
     #Test single image provided returns classification and code 200
     test_img_path = TEST_IMAGES_DIRECTORY / "sample_image.jpg"
     response = client.post('/classify/trupanea', data={'image': (open(test_img_path, 'rb'), test_img_path)})
+
     assert response.status_code == 200
     assert len(response.data) > 0
     assert response.data.count(b'probability') == 3 #Three predictions for one image
@@ -19,14 +21,25 @@ def test_upload_and_get_predictions(client, local_repo):
     images = []
     for image in os.listdir(test_img_path):
         images.append((open(test_img_path / image, 'rb'), test_img_path / image))
-    
     data = {'image': images[0], 'image2': images[1]}
     response = client.post('/classify/trupanea', data=data)
+
     assert response.status_code == 200
     assert len(response.data) > 0
     assert response.data.count(b'probability') == 6 #Three predictions per image
 
     #Test no image returns 400
     response = client.post('/classify/trupanea')
+
     assert response.status_code == 400
     assert b'No image provided' in response.data
+
+def test_get_insect_types(client):
+
+    #Test endpoint returns all insect types
+    num_types = len(os.listdir(globals.ML_MODELS_DIRECTORY))
+    response = client.get('/get_insect_types')
+
+    assert response.status_code == 200
+    assert response.data.count(b'label') == num_types
+    assert response.data.count(b'value') == num_types
