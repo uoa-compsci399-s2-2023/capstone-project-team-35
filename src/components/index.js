c9beae023d6c7236a3d24c0773b6138b23f9130f
Foxdapple <110433393+Foxdapple@users.noreply.github.com>
2023-09-18 17:53:36 +1200

    deleted repo

--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Image_upload} from './image_upload/Image_upload'
-export { default as Insect_type} from './insect_type/Insect_type'
--- src/containers/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Title } from './title/Title'
-export { default as Control_frame } from './control_frame/Control_frame'
