57705a2c92cd1e745b126c74c617744d644e7179
Danila <dkho655@aucklanduni.ac.nz>
2023-10-18 15:11:58 +1300

    Put all data files into data directory

--- flask-backend/app/globals.py
+++ flask-backend/app/globals.py
@@ -1,13 +1,14 @@
 """Application's global variables."""
 from utils import get_project_root
 
-USER_UPLOADED_IMAGES_DIRECTORY              = get_project_root() / "app" / "endpoints" / "upload_and_get_predictions" / "tmp" / "uploaded_images"          
-STANDARDIZED_IMAGES_DIRECTORY               = get_project_root() / "app" / "endpoints" / "upload_and_get_predictions" / "tmp" / "standardized_images"  
-BATCH_PREDICTION_RESULTS_DIRECTORY     = get_project_root() / "app" / "endpoints" / "download_predictions" / "tmp" / "batch_results_file"         
-INDIV_PREDICTION_RESULTS_DIRECTORY     = get_project_root() / "app" / "endpoints" / "download_individual_prediction" / "tmp" / "results_files"                         
-ML_MODELS_DIRECTORY                         = get_project_root() / "app" / "ml" / "models"
-SPECIES_ANNOTATIONS_DIRECTORY               = get_project_root() / "app" / "species_info" / "Annotations"
-SPECIES_SAMPLE_IMAGES_DIRECTORY             = get_project_root() / "app" / "species_info" / "Images"
+USER_UPLOADED_IMAGES_DIRECTORY              = get_project_root() / "app" / "data" / "tmp" / "images" / "user_uploaded_images"
+STANDARDIZED_IMAGES_DIRECTORY               = get_project_root() / "app" / "data" / "tmp" / "images" / "standardized_images"  
+BATCH_PREDICTION_RESULTS_DIRECTORY          = get_project_root() / "app" / "data" / "tmp" / "prediction_results" / "batch_result"
+INDIV_PREDICTION_RESULTS_DIRECTORY          = get_project_root() / "app" / "data" / "tmp" / "prediction_results" / "batch_results"
+ML_MODELS_DIRECTORY                         = get_project_root() / "app" / "data" / "ml" / "models"
+SPECIES_INFO_DIRECTORY                      = get_project_root() / "app" / "data" / "species_info"
+SPECIES_ANNOTATIONS_DIRECTORY               = SPECIES_INFO_DIRECTORY / "Annotations"
+SPECIES_SAMPLE_IMAGES_DIRECTORY             = SPECIES_INFO_DIRECTORY / "Images"
 DEFAULT_MODEL_TYPE                          = "InceptionV3"
 DEFAULT_INSECT_SUPERTYPE                    = "fruitfly"
 TOP_PREDICTIONS_COUNT                       = 10
