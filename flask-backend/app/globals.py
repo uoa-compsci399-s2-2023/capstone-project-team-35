ab5395cb095967688f0b3f3cfa61bf9570fd2234
Danila <dkho655@aucklanduni.ac.nz>
2023-10-18 15:12:11 +1300

    Update globals

--- flask-backend/app/globals.py
+++ flask-backend/app/globals.py
@@ -4,7 +4,7 @@ from utils import get_project_root
 USER_UPLOADED_IMAGES_DIRECTORY              = get_project_root() / "app" / "data" / "tmp" / "images" / "user_uploaded_images"
 STANDARDIZED_IMAGES_DIRECTORY               = get_project_root() / "app" / "data" / "tmp" / "images" / "standardized_images"  
 BATCH_PREDICTION_RESULTS_DIRECTORY          = get_project_root() / "app" / "data" / "tmp" / "prediction_results" / "batch_result"
-INDIV_PREDICTION_RESULTS_DIRECTORY          = get_project_root() / "app" / "data" / "tmp" / "prediction_results" / "batch_results"
+INDIV_PREDICTION_RESULTS_DIRECTORY          = get_project_root() / "app" / "data" / "tmp" / "prediction_results" / "individual_results"
 ML_MODELS_DIRECTORY                         = get_project_root() / "app" / "data" / "ml" / "models"
 SPECIES_INFO_DIRECTORY                      = get_project_root() / "app" / "data" / "species_info"
 SPECIES_ANNOTATIONS_DIRECTORY               = SPECIES_INFO_DIRECTORY / "Annotations"
