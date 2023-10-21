c96fadf05139649ef5402eea590167a87ced4b60
Tommo9182 <tclu830@aucklanduni.ac.nz>
2023-10-21 22:47:36 +1300

    Added pytest to requirements and adjusted conftest

--- flask-backend/tests/conftest.py
+++ flask-backend/tests/conftest.py
@@ -25,8 +25,8 @@ def end_of_tests():
     
     uploaded_images = os.listdir(globals.USER_UPLOADED_IMAGES_DIRECTORY)
     standardized_images = os.listdir(globals.STANDARDIZED_IMAGES_DIRECTORY / "Images")
-    results = os.listdir(globals.BATCH_PREDICTION_RESULTS_DIRECTORY)
-
+    batch_results = os.listdir(globals.BATCH_PREDICTION_RESULTS_DIRECTORY)
+    single_results = os.listdir(globals.INDIV_PREDICTION_RESULTS_DIRECTORY)
     
     if "sample_image.jpg" in uploaded_images:
         os.remove(globals.USER_UPLOADED_IMAGES_DIRECTORY / "sample_image.jpg")
@@ -42,5 +42,12 @@ def end_of_tests():
     if "sample_image3.png" in standardized_images:
         os.remove(globals.STANDARDIZED_IMAGES_DIRECTORY / "Images" / "sample_image3.png")
 
-    if "predictions.csv" in results:
+    if "predictions.csv" in batch_results:
         os.remove(globals.BATCH_PREDICTION_RESULTS_DIRECTORY / "predictions.csv")
+    
+    if "sample_image.jpg_predictions.csv" in single_results:
+        os.remove(globals.INDIV_PREDICTION_RESULTS_DIRECTORY / "sample_image.jpg_predictions.csv")
+    if "sample_image2.jpg_predictions.csv" in single_results:
+        os.remove(globals.INDIV_PREDICTION_RESULTS_DIRECTORY / "sample_image2.jpg_predictions.csv")
+    if "sample_image3.jpg_predictions.csv" in single_results:
+        os.remove(globals.INDIV_PREDICTION_RESULTS_DIRECTORY / "sample_image3.jpg_predictions.csv")
